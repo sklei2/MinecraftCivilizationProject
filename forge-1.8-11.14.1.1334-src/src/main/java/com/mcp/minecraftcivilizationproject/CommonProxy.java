6bd2ddb690b586d6155381b01cb730b6ab79c420
seank <stk1151@g.rit.edu>
2015-05-26 12:04:45 -0400

    Have the common proxy have both preInit and Init methods.

--- forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/minecraftcivilizationproject/CommonProxy.java
+++ forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/minecraftcivilizationproject/CommonProxy.java
@@ -1,9 +1,27 @@
 package com.mcp.minecraftcivilizationproject;
 
+import com.mcp.minecraftcivilizationproject.items.ModItems;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+
 public class CommonProxy {
 
 	public void registerRenderers(){
 		// This is going to be implemented in the clientProxy.
+	}
+	
+	
+	public void preInit(FMLPreInitializationEvent e){
+		ModItems.createItems();
+	}
+
+
+	public void init(FMLInitializationEvent e) {
+		
+		
 	}
 	
 }
