3603590e8cc2139ba3e88e555f04eed07b0e065a
seank <stk1151@g.rit.edu>
2015-05-26 12:06:10 -0400

    Have the client proxy override init in the hopes of registering items and their textures and such. But it still doesn't seem to be the case since I need to figure out when these proxies are actually used.

--- forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/client/ClientProxy.java
+++ forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/client/ClientProxy.java
@@ -1,5 +1,8 @@
 package com.mcp.client;
 
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import com.mcp.client.render.items.ItemRenderRegister;
 import com.mcp.minecraftcivilizationproject.CommonProxy;
 
 public class ClientProxy extends CommonProxy{
@@ -7,5 +10,12 @@ public class ClientProxy extends CommonProxy{
 	@Override
 	public void registerRenderers(){
 		// THIS is where all of the actual renderering for icons and items.
+	}
+	
+	@Override
+	public void init(FMLInitializationEvent e){
+		super.init(e);
+		
+		ItemRenderRegister.registerItemRenderer();
 	}
 }
