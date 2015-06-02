7c63dbfe2f09a8c1fdfae3f5745d65256053e214
seank <stk1151@g.rit.edu>
2015-06-02 17:01:53 -0400

    The spear now hurts the horses more than other animals.

--- forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/minecraftcivilizationproject/items/ItemSpear.java
+++ forge-1.8-11.14.1.1334-src/src/main/java/com/mcp/minecraftcivilizationproject/items/ItemSpear.java
@@ -1,9 +1,12 @@
 package com.mcp.minecraftcivilizationproject.items;
 
 import com.mcp.minecraftcivilizationproject.Reference;
+import com.sun.xml.internal.stream.Entity;
 
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -24,7 +27,21 @@ public class ItemSpear extends ItemSword {
 		this.setUnlocalizedName(Reference.MOD_ID + "_" + name + "_" + this.spearMaterial.name());
 		this.setCreativeTab(CreativeTabs.tabCombat);
 		GameRegistry.registerItem(this, getUnlocalizedName().substring(5));
-		this.attackDamage = 3.0F + material.getDamageVsEntity();
+		this.attackDamage = 3.0F + spearMaterial.getDamageVsEntity();
 	}
+	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+    {
+		if(target instanceof EntityHorse){
+			this.attackDamage = (float) (1.5 * (3.0F + spearMaterial.getDamageVsEntity()));
+			target.setHealth(target.getHealth() - attackDamage);;
+		}
+		else{
+			this.attackDamage = 3.0F + spearMaterial.getDamageVsEntity();
+		}
+        stack.damageItem(1, attacker);
+        return true;
+    }
 	
 }
