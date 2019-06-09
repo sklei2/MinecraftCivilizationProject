bf5a99c1a6e1a3b27dfca2a222752b4e3d1b35ae
RJ <rjd3756@rit.edu>
2019-06-08 20:48:06 -0400

    fixed compilation issues

--- forge-1.12.2-14.23.5.2768-mdk/src/main/java/com/minecraftcivproject/mcp/server/managers/tribe/TribeManager.java
+++ forge-1.12.2-14.23.5.2768-mdk/src/main/java/com/minecraftcivproject/mcp/server/managers/tribe/TribeManager.java
@@ -7,14 +7,16 @@ public class TribeManager {
 
     private final GoalManager goalManager;
     private final String tribeName;
+    private final TownBlueprint townBlueprint;
 
     public TribeManager(String tribeName, TownBlueprint townBlueprint){
         this(new GoalManager(townBlueprint), tribeName, townBlueprint);
     }
 
-    public TribeManager(GoalManager goalManager, String tribeName){
+    public TribeManager(GoalManager goalManager, String tribeName, TownBlueprint townBlueprint){
         this.goalManager = goalManager;
         this.tribeName = tribeName;
+        this.townBlueprint = townBlueprint;
     }
 
     public String getTribeName() {
