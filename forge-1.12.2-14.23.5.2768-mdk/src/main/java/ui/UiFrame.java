3a3a6695ec09a3145f197851f6a0933589ab97d1
RJ <rjd3756@rit.edu>
2020-11-21 18:16:41 -0500

    added some ui components

---
+++ forge-1.12.2-14.23.5.2768-mdk/src/main/java/ui/UiFrame.java
@@ -0,0 +1,29 @@
+package ui;
+
+import ui.tribe.general.TribesUi;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UiFrame extends JFrame {
+
+    private final NavigationBar navigationBar;
+    private final MainContentPane mainContentPane;
+
+    public UiFrame(){
+        setTitle("Tribes");
+        setSize(1200, 900);
+
+        setLayout(new BorderLayout());
+
+        mainContentPane = new MainContentPane(new TribesUi(), new JPanel());
+        navigationBar = new NavigationBar(mainContentPane);
+
+        navigationBar.setSize(200, 900);
+        mainContentPane.setSize(1000, 900);
+
+        add(navigationBar, BorderLayout.WEST);
+
+        add(mainContentPane, BorderLayout.CENTER);
+    }
+}
